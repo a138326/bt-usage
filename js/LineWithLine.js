a53911a0b06a3d84277b4d222faf37246b37bf4b
a138326 <pmaclaren@agl.com.au>
2020-04-01 00:32:10 +1100

    Upload 01

---
+++ js/LineWithLine.js
@@ -0,0 +1,25 @@
+Chart.defaults.LineWithLine = Chart.defaults.line;
+
+Chart.controllers.LineWithLine = Chart.controllers.line.extend({
+   draw: function(ease) {
+      Chart.controllers.line.prototype.draw.call(this, ease);
+
+      if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
+         var activePoint = this.chart.tooltip._active[0],
+             ctx = this.chart.ctx,
+             x = activePoint.tooltipPosition().x,
+             topY = this.chart.legend.bottom,
+             bottomY = this.chart.chartArea.bottom;
+
+         // draw line
+         ctx.save();
+         ctx.beginPath();
+         ctx.moveTo(x, topY);
+         ctx.lineTo(x, bottomY);
+         ctx.lineWidth = 2;
+         ctx.strokeStyle = '#fff';
+         ctx.stroke();
+         ctx.restore();
+      }
+   }
+});
